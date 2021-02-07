fbb08e9c591459f99bf4c3d1bf60bcfcdb3eaa24
andaccc <8611553+andaccc@users.noreply.github.com>
2021-02-07 20:05:23 +0800

    add drop files register

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Development Plan
     - https://github.com/electron-react-boilerplate/electron-react-boilerplate
 - image drag-drop
 - value analyzer
+- simple image edit
+- UI design
+- leftside file explorer
+- crtl c, v paste image 
+- multiple image
+- top menu bar
+- save/load page
+- tag page?
--- src/renderer/components/ImageViewer.jsx
+++ src/renderer/components/ImageViewer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import ReactDOM from 'react-dom'
+
+const { ipcRenderer } = require('electron')
 
 
 //https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+//https://medium.com/@650egor/simple-drag-and-drop-file-upload-in-react-2cb409d88929
 
+// image drag drop use native api instead !!!!
+// https://www.electronjs.org/docs/tutorial/native-file-drag-drop
 
 const defaultProps = {
+  borderRadius: "borderRadius",
   bgcolor: '#424242',
   borderColor: 'grey.500',
   m: 5,
@@ -16,20 +23,56 @@ const defaultProps = {
   justifyContent: "center"
 };
 
+function preventDefaults(e) {
+  e.preventDefaults();
+  e.stopPropagation();
+}
+
+function handleDropImage(e) {
+  console.log(e)
+  let reader = new FileReader()
+  reader.readAsDataURL(e.dataTransfer.files)
+  reader.onloadend = () => {
+    let img = document.createElement('img');
+    img.src = reader.result;
+    this.viewZone.appendChild(img)
+  }
+}
+
 export default class ImageViewer extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
 
     }
+
+    this.dropRef = React.createRef();
+  }
+
+
+
+  componentDidMount() {
+    document.ondragover = function(e) {
+      // need this for drop file to work
+      e.preventDefault(); 
+    };
+    
+
+    this.dropRef.current.addEventListener('drop', (events) => {
+      console.log('dropped');
+
+      //handleDropImage(events);
+    });
   }
 
   render() {
     return (
       <Container>
-        <Box borderRadius="borderRadius" {...defaultProps} >
+        <Box ref={this.dropRef} id="dropZone" {...defaultProps} >
           Drag to here
         </Box>
+        <div ref={this.viewZone}>
+        </div>
       </Container>
     );
   }
